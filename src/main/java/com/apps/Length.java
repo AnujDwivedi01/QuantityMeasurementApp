1bcf8ce6b9c17af67bc131cae1fcab5024462689
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-02-22 16:53:22 +0530

    [Anuj] : completed UC$ task

--- src/main/java/com/apps/Length.java
+++ src/main/java/com/apps/Length.java
@@ -1,3 +1,4 @@
+
 package com.apps;
 
 public class Length {
@@ -7,17 +8,20 @@ public class Length {
 
 	// Enum inside class
 	public enum LengthUnit {
-		FEET(12.0), INCHES(1.0);
+	    FEET(12.0),
+	    INCHES(1.0),
+	    YARDS(36.0),
+	    CENTIMETERS(0.393701);
 
-		private final double conversionFactor;
+	    private final double conversionFactor;
 
-		LengthUnit(double conversionFactor) {
-			this.conversionFactor = conversionFactor;
-		}
+	    LengthUnit(double conversionFactor) {
+	        this.conversionFactor = conversionFactor;
+	    }
 
-		public double getConversionFactor() {
-			return conversionFactor;
-		}
+	    public double getConversionFactor() {
+	        return conversionFactor;
+	    }
 	}
 
 	// Constructor
