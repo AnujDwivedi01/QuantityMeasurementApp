7c5c9c78f30489e232907fd0a7d674e260981965
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-03-17 14:52:58 +0530

    [Anuj] : completed UC 16 task

---
+++ src/main/java/com/apps/exception/DatabaseException.java
@@ -0,0 +1,12 @@
+
+package com.apps.exception;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message, Throwable cause) {
+
+        super(message, cause);
+
+    }
+
+}
