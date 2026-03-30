7350e442e7c0762a9d4f509987adbb1043e21c7d
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-03-30 15:04:15 +0530

    [Anuj] : completed UC 18 task done withjwt security

---
+++ src/main/java/com/app/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+
+package com.app.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String message;
+}
